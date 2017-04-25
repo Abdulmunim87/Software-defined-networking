181f63436a2002faf34a89c3422b2f1c5acc9657
Swapnasheel <Swapnasheel>
2017-04-25 03:17:05 -0700

    Added SET functionality..

--- App.py
+++ App.py
@@ -1,18 +1,21 @@
 import httplib2
 import json
 import sys
+from collections import OrderedDict
 
 h = httplib2.Http(".cache")
 h.add_credentials('admin', 'admin')
 
-# Uncommented below line, use it on your needs.. #swapnasheel
-#operation = raw_input("Enter The Operation index: ")
+ip = ''
+oid = ''
+community = ''
+value = ''
 
 ####################### Get Network Topology #########################
 
 def topology():
 	url = "http://localhost:8181/restconf/operational/network-topology:network-topology"
-	print url
+	#print url
 
 	print
 	resp, content = h.request(url, "GET")
@@ -34,11 +37,11 @@ def topology():
 		print "Link ID - ", topology['link'][i]['link-id']
 		i += 1
 
-####################### Get Port Statistics #########################
+####################### Get Link Statistics ###########################
 
-def portStats():
+def linkStats():
 	switch = raw_input("Enter the Switch Number: ")
-	port = raw_input("Enter the port number wrt the switch given above: ")
+	link = raw_input("Enter the link number wrt the switch given above: ")
 	url1 = 'http://127.0.0.1:8181/restconf/operational/opendaylight-inventory:nodes/node/openflow:'
 	url2 =  '/node-connector/openflow:'
 	url = url1+switch+url2+switch+':'+port
@@ -46,14 +49,21 @@ def portStats():
 
 	print
 	resp, content = h.request(url, "GET")
-	portStats = json.loads(content)
+	linkStats = json.loads(content)
 	
-	stats1 = portStats['node-connector'][0]['id']
-	stats2 = portStats['node-connector'][0]['opendaylight-port-statistics:flow-capable-node-connector-statistics']
-	stats3 = portStats['node-connector'][0]['address-tracker:addresses'][0]
+	stats1 = linkStats['node-connector'][0]
+	stats2 = linkStats['node-connector'][0]['opendaylight-port-statistics:flow-capable-node-connector-statistics']
+	stats3 = linkStats['node-connector'][0]['address-tracker:addresses'][0]
+
 
 	if portStats:
-		print "Node connector - ", stats1
+		print "Node connector - ", stats1['id']
+	
+		print "Flow-node-name - ", stats1['flow-node-inventory:name']
+
+		print "Flow-node-speed - ", stats1['flow-node-inventory:current-speed']
+
+		print "Link feature - ", stats1['flow-node-inventory:current-feature']
 
 		print "Bytes received - ", stats2['bytes']['received']
 
@@ -62,23 +72,22 @@ def portStats():
 		print "Packets received - ", stats2['packets']['received']
 
 		print "Packets transmitted - ", stats2['packets']['transmitted']
-
-		print "MAC address of the host connected thru the node-connector: - ", stats3['mac']
-
-		print "IP address of the host connected thru the node-connector: - ", stats3['ip']
+	
+		print
+		print "Link State: ", json.dumps(stats1['flow-node-inventory:state'])
 		print
 
 	else:
 		print "No data found"
 		print
-########################## SNMP-GET ################################
+		
+########################## SNMP-GET ##################################
 
 def snmpGET():
-	ip = raw_input("Please enter the IP address: ")
-	oid = raw_input("Please enter the OID: ")
-	get = raw_input("Please enter the get-type: ")
-	community = raw_input("Please enter the community: ")
-
+        global ip,oid,community,value
+        
+	getParam()
+        
 	url = 'http://127.0.0.1:8181/restconf/operations/snmp:snmp-get'
 	Param = {
 		    "input":
@@ -102,7 +111,7 @@ def snmpGET():
 		Info = json.loads(content)
 		for key in Info:
 			if key == 'errors':
-				print "Invalid data provided"
+				print "INVALID DATA PROVIDED"
 				print
 				break
 			else:	
@@ -111,28 +120,216 @@ def snmpGET():
 					count = len(Info['output']['results'])	
 					for i in range(count):
 			    			print 'OID\t\t\t\t\t\tValue'
-						print Info['output']['results'][i]['oid'],'\t\t',Info['output']['results'][i]['value']
-						print
+						print Info['output']['results'][i]['oid'],'\t\t\t',Info['output']['results'][i]['value']
+						
 				else:
 					print 'No output for the OID!'
 					print
 	except ValueError as err:
 		print "ERROR: ", err
-#########################################################
+
+
+####################### Get Parameters ############################################
+		
+def getParam():
+        global ip,oid,community,value
+
+        ip = raw_input("Please enter the IP address: ")
+	oid = raw_input("Please enter the OID: ")
+	community = raw_input("Please enter the community: ")
+	value = raw_input("Please enter the new value: ")
+
+######################### SNMP-SET #################################################
+
+def snmpSET():
+        global ip,oid,community,value
+        
+	getParam()
+        
+	url = 'http://127.0.0.1:8181/restconf/operations/snmp:snmp-set'
+	Param = {
+		    "input":
+		            {
+		                "ip-address": str(ip),
+		                "oid": str(oid),
+		                "community": community,
+				"value": value
+	    }			
+	}
+	 
+	resp, content = h.request(
+	    uri = url,
+	    method = 'POST',
+	    headers={'Content-Type':'application/json'},
+	    body=json.dumps(Param)
+	    )
+
+	print
+		
+	print "Value Set!"
+
+####################### SYSTEM STATUS #############################################
+
+def sysStatus():
+	url = 'http://127.0.0.1:8181/restconf/operations/snmp:snmp-get'
+
+	system = [('System Description', '1.3.6.1.2.1.1.1.0'),
+		('Local Time', '1.3.6.1.4.1.2021.100.4.0'),
+		('System Uptime', '1.3.6.1.2.1.1.3.0')]
+
+	CPU = [('1-minute Load average', '1.3.6.1.4.1.2021.10.1.3.1'),
+		('5-minute Load average', '1.3.6.1.4.1.2021.10.1.3.2'),
+		 ('15-minute Load average', '1.3.6.1.4.1.2021.10.1.3.3')]
+
+	memory = [('Total', '1.3.6.1.4.1.2021.4.5.0'),
+		('Available', '1.3.6.1.4.1.2021.4.6.0'),
+		('Buffered', '1.3.6.1.4.1.2021.4.14.0'),
+		('Cached', '1.3.6.1.4.1.2021.4.15.0')]
+
+	disk = [('Path where the disk is mounted', '1.3.6.1.4.1.2021.9.1.2.1'),
+		('Path of the device for the partition', '1.3.6.1.4.1.2021.9.1.3.1'),
+		('Total size of the disk/partion (kBytes)', '1.3.6.1.4.1.2021.9.1.6.1'),
+		('Available space on the disk', '1.3.6.1.4.1.2021.9.1.7.1'),
+		('Used space on the disk', '1.3.6.1.4.1.2021.9.1.8.1'),
+		('% space used on disk', '1.3.6.1.4.1.2021.9.1.9.1')]
+
+	system = OrderedDict(system)
+	memory = OrderedDict(memory)
+	disk = OrderedDict(disk)
+	CPU = OrderedDict(CPU)
+	print '##############################################################################################################'
+	print
+	print "######################## System Info ###########################"
+	for oid in system:
+		Param = {
+			    "input":
+				    {
+				        "ip-address": "127.0.0.1",
+				        "oid" : system[oid],
+				        "get-type" : "GET",
+				        "community" : "public"
+		    }
+		}
+	 
+		resp, content = h.request(
+		    uri = url,
+		    method = 'POST',
+		    headers={'Content-Type':'application/json'},
+		    body=json.dumps(Param)
+		    )
+	 
+		Info = json.loads(content)
+		print
+		count = len(Info['output']['results'])	
+		for i in range(count):
+			print  oid + " - ", Info['output']['results'][i]['value']
+	###################################################################################
+	print
+	print
+	print "############################ CPU Info ##########################"
+	for oid in CPU:
+		Param = {
+			    "input":
+				    {
+				        "ip-address": "127.0.0.1",
+				        "oid" : CPU[oid],
+				        "get-type" : "GET",
+				        "community" : "public"
+		    }
+		}
+	 
+		resp, content = h.request(
+		    uri = url,
+		    method = 'POST',
+		    headers={'Content-Type':'application/json'},
+		    body=json.dumps(Param)
+		    )
+	 
+		Info = json.loads(content)
+		print
+		count = len(Info['output']['results'])	
+		for i in range(count):
+			print  oid + " - ", Info['output']['results'][i]['value']
+	#################################################################################
+	print
+	print
+	print "########################### Memory Info (kB) #####################"
+	for oid in memory: 
+		Param = {
+			    "input":
+				    {
+				        "ip-address": "127.0.0.1",
+				        "oid" : memory[oid],
+				        "get-type" : "GET",
+				        "community" : "public"
+		    }
+		}
+	 
+		resp, content = h.request(
+		    uri = url,
+		    method = 'POST',
+		    headers={'Content-Type':'application/json'},
+		    body=json.dumps(Param)
+		    )
+	 
+		Info = json.loads(content)
+		print
+		count = len(Info['output']['results'])	
+		for i in range(count):
+			print  oid + " - ", Info['output']['results'][i]['value']
+
+	##################################################################################
+	print
+	print
+	print "############################ Disk Info (kB) #######################"
+	for oid in disk: 
+		Param = {
+			    "input":
+				    {
+				        "ip-address": "127.0.0.1",
+				        "oid" : disk[oid],
+				        "get-type" : "GET",
+				        "community" : "public"
+		    }
+		}
+	 
+		resp, content = h.request(
+		    uri = url,
+		    method = 'POST',
+		    headers={'Content-Type':'application/json'},
+		    body=json.dumps(Param)
+		    )
+	 
+		Info = json.loads(content)
+		print
+		count = len(Info['output']['results'])	
+		for i in range(count):
+			print  oid + " - ", Info['output']['results'][i]['value']
+	print '#################################################################################################################'
+	
+
+#####################################################################
 
 def Exit():
-	print "Exiting..!!"
+	print	
+	print "Exiting.."
+	print
 	sys.exit()
 
-
+while 1:
 #Added swicth case Input type
 
-options = {     1: topology,
-		2: portStats,
-		3: snmpGET,
-		4: Exit,
-}
+	options = {     1: topology,
+			2: linkStats,
+			3: sysStatus,
+			4: snmpGET,
+			5: snmpSET,
+			6: Exit,
+	}
+	print
+	print "Please select operation : \n1. Topology \n2. Link Statistics \n3. System Status \n4. Use SNMP-GET \n5. Use SNMP-SET \n6. Exit \n" 
+	num=input("Enter here: ")
+	options[num]()
 
-print "Please select operation : \n1. Topo \n2. PortStats \n3. SNMPGET \n4. Exit \n \n" 
-num=input()
-options[num]()
+
+	
